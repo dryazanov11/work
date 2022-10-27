510b1d5e4cc186d82d2041cc5fed4bc3df7476de
User@A0068 <User@A0068>
2022-10-27 08:29:39 +0300

    Обновил тесты для test_commands

--- tests_tm_core/test_commands.py
+++ tests_tm_core/test_commands.py
@@ -10,7 +10,6 @@ class TestValidator(BaseCase):
 
         self.noname_validator = "{'messageOnError':'Message','areaId':'cbad64d2-eb21-4c4f-9ea3-243c26f7fca2','schemaId':'c4100298-c735-48cb-996f-a8f43a1aa646','expression':'true'}"
         self.nomessage_error_validator = "{'name':'name','areaId':'cbad64d2-eb21-4c4f-9ea3-243c26f7fca2','schemaId':'c4100298-c735-48cb-996f-a8f43a1aa646','expression':'true'}"
-        self.noarea_validator = "{'name':'name','messageOnError':'Message','schemaId':'c4100298-c735-48cb-996f-a8f43a1aa646','expression':'true'}"
         self.expression_validator = "{'name':'name','messageOnError':'Message','schemaId':'c4100298-c735-48cb-996f-a8f43a1aa646','areaId':'cbad64d2-eb21-4c4f-9ea3-243c26f7fca2'}"
 
         self.create_validator = "{\"Name\":\"autocheck\",\"Expression\":\"('{$$system}' = 'N3 1a55e61b-dd7a-4acf-a94a-37d21d761af5')\",\"MessageOnError\":\"test_error\",\"AreaId\":\"cbad64d2-eb21-4c4f-9ea3-243c26f7fca2\",\"SchemaId\":\"c4100298-c735-48cb-996f-a8f43a1aa646\"}"
@@ -27,32 +26,35 @@ class TestValidator(BaseCase):
         #без messageOnError
         nomessage_error = MyRequests.post('/tm-core/api/Commands/RegisterValidator', headers={'Content-Type': 'application/json-patch+json', 'Authorization': f'{config.token_tm_core}'},
                                           data=self.nomessage_error_validator)
-        Assertions.assert_json_value_by_name(nomessage_error, 'message', "Value cannot be null.\nParameter name: messageOnError",
+        Assertions.assert_json_value_by_name(nomessage_error, 'message', "MessageOnError is required parameter\nParameter name: messageOnError",
                                              'Неожиданная ошибка при создании внутреннего валидатора без messageOnError')
-
-        #нет areaId - ЗАВЕДЕНА ЗАДАЧА https://jira.netrika.ru/browse/TELEMED-2133
-        noarea = MyRequests.post('/tm-core/api/Commands/RegisterValidator',headers={'Content-Type': 'application/json-patch+json','Authorization': f'{config.token_tm_core}'},
-                                       data=self.noarea_validator)
-        Assertions.assert_json_value_by_name(noarea, 'message', "23503: INSERT или UPDATE в таблице \"validator\" нарушает ограничение внешнего ключа \"area_id\"",
-                                             'Неожиданная ошибка при создании внутреннего валидатора без area_id')
 
         #нет expression
         noexpression = MyRequests.post('/tm-core/api/Commands/RegisterValidator',headers={'Content-Type': 'application/json-patch+json','Authorization': f'{config.token_tm_core}'},
                                        data=self.expression_validator)
-        Assertions.assert_json_value_by_name(noexpression, 'message', "Value cannot be null.\nParameter name: expression",'Неожиданная ошибка при создании внутреннего валидатора без expression')
+        Assertions.assert_json_value_by_name(noexpression, 'message', "Expression is required parameter\nParameter name: expression",
+                                             'Неожиданная ошибка при создании внутреннего валидатора без expression')
 
         #проверка дубликата имени
         dublicate = MyRequests.post('/tm-core/api/Commands/RegisterValidator',headers={'Content-Type': 'application/json-patch+json','Authorization': f'{config.token_tm_core}'},
                                        data=self.create_dublicate_name_validator)
         Assertions.assert_json_value_by_name(dublicate, 'message', 'Duplicate validator name', 'При дубликате имени валидатора получена неожидаемая ошибка')
 
-        #обновление с несуществующим id - https://jira.netrika.ru/browse/TELEMED-2133
+        #обновление с несуществующим id
+        unexist_id_update = MyRequests.post(f'/tm-core/api/Commands/UpdateValidator/{config.default_id}',headers={'Content-Type': 'application/json-patch+json','Authorization': f'{config.token_tm_core}'},
+                                       data=self.create_validator)
+        Assertions.assert_json_value_by_name(unexist_id_update, 'message', 'Validator with id = 3fa85f64-5717-4562-b3fc-2c963f66afa6 not found.',
+                                             'Ожидаемая ошибка не получена')
 
         #получение несуществующего id
         get_validator = MyRequests.get(f'/tm-core/api/Queries/GetValidator/{config.default_id}',headers={'Authorization': f'{config.token_tm_core}'})
         Assertions.assert_json_value_by_name(get_validator, 'message', f"Validator '{config.default_id}' not found",'Неожиданное сообщение об ошибке')
 
-        #удаление несуществующего id - https://jira.netrika.ru/browse/TELEMED-2133
+        #удаление несуществующего id
+        unexist_id_delete = MyRequests.delete(f'/tm-core/api/Commands/DeleteValidator/{config.default_id}',headers={'Content-Type': 'application/json-patch+json',
+                                                     'Authorization': f'{config.token_tm_core}'})
+        Assertions.assert_json_value_by_name(unexist_id_delete, 'message','Validator with id = 3fa85f64-5717-4562-b3fc-2c963f66afa6 not found.',
+                                             'Ожидаемая ошибка не получена')
 
     @allure.feature("Создание/получение/обновление/удаление валидатора")
     def test_create_update_get_delete_validator(self):
@@ -74,7 +76,7 @@ class TestValidator(BaseCase):
         Assertions.assert_expectedvalue_equal_receivedvalue(get, validator_id, get.json()['result']['id'], 'Информация, полученная в ответе не соответствует запрошенному ID валидатора')
 
         delete = MyRequests.delete(f'/tm-core/api/Commands/DeleteValidator/{validator_id}', headers={'Authorization': f'{config.token_tm_core}'})
-        Assertions.assert_json_value_by_name(delete, 'success', True, 'Удаление внешнего валидатора прошло неуспешно')
+        Assertions.assert_json_value_by_name(delete, 'success', True, 'Удаление валидатора прошло неуспешно')
 
         get_all = MyRequests.get('/tm-core/api/Queries/GetValidators?take=100', headers={'Authorization': f'{config.token_tm_core}'})
         Assertions.assert_json_value_by_name(get_all, 'success', True, 'Метод получения всех валидаторов ответил неуспешно')
@@ -97,30 +99,31 @@ class TestExternalValidator(BaseCase):
         #создание внешнего валидатора без name
         noname = MyRequests.post('/tm-core/api/Commands/RegisterExternalValidator', headers={'Content-Type': 'application/json-patch+json', 'Authorization': f'{config.token_tm_core}'},
                                  data=self.no_name_exvalidator)
-        Assertions.assert_json_value_by_name(noname, 'message', 'validatorName is required parameter', 'Неожиданная ошибка при создании внутреннего валидатора без name')
+        Assertions.assert_json_value_by_name(noname, 'message', 'validatorName is required parameter', 'Неожиданная ошибка при создании внешнего валидатора без name')
 
-        #не передан url - https://jira.netrika.ru/browse/TELEMED-2133
+        #не передан url
         nourl = MyRequests.post('/tm-core/api/Commands/RegisterExternalValidator', headers={'Content-Type': 'application/json-patch+json', 'Authorization': f'{config.token_tm_core}'},
                                  data=self.no_url_exvalidator)
-        Assertions.assert_json_value_by_name(nourl, 'message', "Value cannot be null.\nParameter name: url", 'Неожиданная ошибка при создании внутреннего валидатора без url')
+        Assertions.assert_json_value_by_name(nourl, 'message', "Url is required parameter\nParameter name: url",
+                                             'Неожиданная ошибка при создании внешнего валидатора без url')
 
-        #не передан MessageOnError - https://jira.netrika.ru/browse/TELEMED-2133
+        #не передан MessageOnError
         nomessage = MyRequests.post('/tm-core/api/Commands/RegisterExternalValidator', headers={'Content-Type': 'application/json-patch+json', 'Authorization': f'{config.token_tm_core}'},
                                  data=self.no_message_exvalidator)
-        Assertions.assert_json_value_by_name(nomessage, 'message', "Value cannot be null.\nParameter name: messageOnError", 'Неожиданная ошибка при создании внутреннего валидатора без messageOnError')
-
-        #не передан areaid - https://jira.netrika.ru/browse/TELEMED-2133
-        noarea = MyRequests.post('/tm-core/api/Commands/RegisterExternalValidator', headers={'Content-Type': 'application/json-patch+json', 'Authorization': f'{config.token_tm_core}'},
-                                 data=self.no_area_exvalidator)
-        Assertions.assert_json_value_by_name(noarea, 'message', "23503: INSERT или UPDATE в таблице \"validator\" нарушает ограничение внешнего ключа \"area_id\"",
-                                             'Неожиданная ошибка при создании внутреннего валидатора без area_id')
+        Assertions.assert_json_value_by_name(nomessage, 'message', "MessageOnError is required parameter\nParameter name: messageOnError",
+                                             'Неожиданная ошибка при создании внутреннего валидатора без messageOnError')
 
         #дубликат имени
         dublicate = MyRequests.post('/tm-core/api/Commands/RegisterExternalValidator', headers={'Content-Type': 'application/json-patch+json', 'Authorization': f'{config.token_tm_core}'},
                                  data=self.dublicate_name_exvalidator)
         Assertions.assert_json_value_by_name(dublicate, 'message', "Duplicate validator name", 'Неожиданная ошибка при дубликате имени')
 
-        #обновление с несуществующим id - https://jira.netrika.ru/browse/TELEMED-2133
+        #обновление с несуществующим id
+        unexist_id_update = MyRequests.post(f'/tm-core/api/Commands/UpdateExternalValidator/{config.default_id}',headers={'Content-Type': 'application/json-patch+json','Authorization': f'{config.token_tm_core}'},
+                                            data=self.create_exvalidator)
+        Assertions.assert_json_value_by_name(unexist_id_update, 'message',
+                                             'Validator with id = 3fa85f64-5717-4562-b3fc-2c963f66afa6 not found.',
+                                             'Ожидаемая ошибка не получена')
 
     @allure.feature("Создание/получение/обновление/удаление внешнего валидатора")
     def test_create_update_get_delete_validator(self):
@@ -143,7 +146,6 @@ class TestExternalValidator(BaseCase):
         delete = MyRequests.delete(f'/tm-core/api/Commands/DeleteValidator/{validator_id}',headers={'Authorization': f'{config.token_tm_core}'})
         Assertions.assert_json_value_by_name(delete, 'success', True, 'Удаление внешнего валидатора прошло неуспешно')
 
-
 @allure.epic("Полноценные проверки Commands")
 class TestCallback(BaseCase):
 
@@ -162,21 +164,22 @@ class TestCallback(BaseCase):
         noname = MyRequests.post('/tm-core/api/Commands/RegisterCallback', headers={'Content-Type': 'application/json-patch+json', 'Authorization': f'{config.token_tm_core}'},data=self.noname)
         Assertions.assert_json_value_by_name(noname, 'message', 'callbackName is required parameter', 'Неожиданная ошибка при создании callback без name')
 
-        #без url - https://jira.netrika.ru/browse/TELEMED-2133
+        #без url
         nourl = MyRequests.post('/tm-core/api/Commands/RegisterCallback',headers={'Content-Type': 'application/json-patch+json','Authorization': f'{config.token_tm_core}'},data=self.nourl)
-        Assertions.assert_json_value_by_name(nourl, 'message', 'Value cannot be null.\nParameter name: url','Неожиданная ошибка при создании callback без url')
+        Assertions.assert_json_value_by_name(nourl, 'message', 'Url is required parameter\nParameter name: url',
+                                             'Неожиданная ошибка при создании callback без url')
 
         #дубликат названия
         dublicate = MyRequests.post('/tm-core/api/Commands/RegisterCallback', headers={'Content-Type': 'application/json-patch+json', 'Authorization': f'{config.token_tm_core}'},data=self.create_callback)
         Assertions.assert_json_value_by_name(dublicate, 'message', 'Duplicate callback name', 'Неожиданная ошибка при создании callback с дублем name')
 
-        #обновление с несуществующим id - https://jira.netrika.ru/browse/TELEMED-2133
+        #обновление с несуществующим id
         incorrect_update = MyRequests.post(f'/tm-core/api/Commands/UpdateCallback/{config.default_id}', headers={'Content-Type': 'application/json-patch+json', 'Authorization': f'{config.token_tm_core}'},data="{}")
-        Assertions.assert_json_value_by_name(incorrect_update, 'message', f'Callback with id ${config.default_id} not found', 'Неожиданная ошибка при обновлении callback с несуществующим ID')
+        Assertions.assert_json_value_by_name(incorrect_update, 'message', f"Callback with id '{config.default_id}' not found", 'Неожиданная ошибка при обновлении callback с несуществующим ID')
 
-        #удаление с несуществующим id - https://jira.netrika.ru/browse/TELEMED-2133
+        #удаление с несуществующим id
         incorrect_delete = MyRequests.delete(f'/tm-core/api/Commands/DeleteCallback/{config.default_id}',headers={'Authorization': f'{config.token_tm_core}'})
-        Assertions.assert_json_value_by_name(incorrect_delete, 'message',f'Callback with id ${config.default_id} not found','Неожиданная ошибка при удалении callback с несуществующим ID')
+        Assertions.assert_json_value_by_name(incorrect_delete, 'message',f"Callback with id '{config.default_id}' not found",'Неожиданная ошибка при удалении callback с несуществующим ID')
 
         #поиск несуществующего callback
         incorrect_get = MyRequests.get(f'/tm-core/api/Queries/GetCallback/{config.default_id}', headers={'Authorization': f'{config.token_tm_core}'})
