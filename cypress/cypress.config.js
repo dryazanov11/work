1459095b24efda5a551d481a8e3b4b479071c7c2
User@A0068 <User@A0068>
2023-05-04 13:12:17 +0300

    Revert "Initial commit"
    This reverts commit 59a110a9147cdeb8af7f00d066da9fb696d57ef9.

--- cypress/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  projectId: 'v3xf2m',
-  e2e: {
-    baseUrl: 'http://r78-test.zdrav.netrika.ru/tm.doctorportal.ui/',
-  },
-})
-
